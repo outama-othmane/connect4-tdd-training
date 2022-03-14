b46e0f4d12845f1d6e15c73cde5894c3b0c71b42
Othmane OUTAMA <outama.othmane@gmail.com>
2022-03-14 14:51:05 +0100

    Step-2: Refactored the Grid class

--- src/main/java/com/octo/connect4/Grid.java
+++ src/main/java/com/octo/connect4/Grid.java
@@ -1,8 +1,21 @@
 package com.octo.connect4;
 
 public class Grid<T> {
+    private Integer[][] matrix;
+
+    public Grid() {
+        int rows = 6;
+        int cols = 7;
+
+        this.matrix = new Integer[6][7];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
     public Integer[][] getMatrix() {
-        Integer[][] matrix = {{0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}};
         return matrix;
     }
 }
--- src/test/java/com/octo/connect4/GridTest.java
+++ src/test/java/com/octo/connect4/GridTest.java
@@ -23,4 +23,6 @@ public class GridTest {
 
         Assert.assertEquals(expected, matrix);
     }
+
+
 }
